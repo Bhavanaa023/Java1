9c2f7fd5e4c2c0c8e8a00d5defd23e5845a968f6
Bhavanaa023 <bhavana.23cs023@sode-edu.in>
2024-10-15 15:34:31 +0530

    USN Numbers

---
+++ B15.java
@@ -0,0 +1,8 @@
+public class B15 {
+    public static void main(String[] args) {
+        String prefix = "4MW23CS";
+        for(int i=1; i<=65; i++) {
+            System.out.println(prefix + String.format("%03d",i));
+        }
+    }
+}
