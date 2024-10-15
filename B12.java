7f7283a0e786c202d7adaf94d3ea84f86bfc6bf2
Bhavanaa023 <bhavana.23cs023@sode-edu.in>
2024-10-15 15:33:40 +0530

    Celsius to Fahrenheit

---
+++ B12.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+public class B12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double tempc=100;
+        double tempf=(tempc*9/5)+32;
+        System.out.println("Temperature in fehrenheit "+tempf);
+        
+    }
+    
+}
