0292e81d468efebeacfa451138fc4c7d19e8016b
Bhavanaa023 <bhavana.23cs023@sode-edu.in>
2024-10-15 15:38:56 +0530

    Data Type Values

---
+++ B16.java
@@ -0,0 +1,11 @@
+public class B16 {
+    public static void main(String[] args) {
+        System.out.println("Data Type\t Default Value\t Max Value\t Min Value ");
+        System.out.println("byte\t" + (byte)0 + "\t" + Byte.MAX_VALUE + "\t" + Byte.MIN_VALUE); 
+        System.out.println("short\t" + (short)0 + "\t" + Short.MAX_VALUE + "\t" + Short.MIN_VALUE);
+        System.out.println("int\t" + (int)0 + "\t" + Integer.MAX_VALUE + "\t" + Integer.MIN_VALUE);
+        System.out.println("long\t" + (long)0 + "\t" + Long.MAX_VALUE + "\t" + Long.MIN_VALUE);
+        System.out.println("float\t" + (float)0 + "\t" + Float.MAX_VALUE + "\t" + Float.MIN_VALUE);
+        System.out.println("double\t" + (double)0 + "\t" + Double.MAX_VALUE + "\t" + Double.MIN_VALUE);
+    }
+}
